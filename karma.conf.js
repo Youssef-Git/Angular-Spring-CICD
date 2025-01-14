0df812dd6f32c7fe275802e50b9f3e3d322e3f32
admin <youssef@Ubuntu2.myguest.virtualbox.org>
2025-01-14 20:03:13 +0100

    New

--- karma.conf.js
+++ karma.conf.js
@@ -7,10 +7,11 @@ module.exports = function(config) {
       require('karma-chrome-launcher'),
       require('karma-jasmine-html-reporter'),
       require('karma-coverage'),
+      require('karma-junit-reporter'), // Ajouter le plugin junit reporter pour GitLab
       require('@angular-devkit/build-angular/plugins/karma')
     ],
     client: {
-      clearContext: false
+      clearContext: false // Permet de ne pas effacer les résultats des tests dans la fenêtre du navigateur
     },
     coverageReporter: {
       dir: require('path').join(__dirname, './coverage'),
@@ -20,13 +21,18 @@ module.exports = function(config) {
         { type: 'text-summary' }
       ]
     },
-    reporters: ['progress', 'kjhtml'],
+    reporters: ['progress', 'kjhtml', 'junit'], // Ajout du reporter junit
+    junitReporter: {
+      outputDir: 'test-results',  // Répertoire où les résultats seront sauvegardés
+      outputFile: 'test-results.xml',  // Nom du fichier XML de sortie
+      useBrowserName: false        // Ne pas inclure le nom du navigateur dans le rapport
+    },
     port: 9876,
     colors: true,
     logLevel: config.LOG_INFO,
-    autoWatch: false,
-    browsers: ['ChromiumHeadlessCI'],
-    singleRun: true,
+    autoWatch: false, // Désactive le mode watch pour l'exécution en CI
+    browsers: ['ChromiumHeadlessCI'],  // Utilise ChromiumHeadlessCI
+    singleRun: true, // Les tests s'exécutent une seule fois
     restartOnFileChange: false,
     customLaunchers: {
       ChromiumHeadlessCI: {
