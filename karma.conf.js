f375b890eb5fb823e23f4e266f9645cd58591db6
admin <youssef@Ubuntu2.myguest.virtualbox.org>
2025-01-31 18:35:32 +0100

    Pipeline10

--- karma.conf.js
+++ karma.conf.js
@@ -1,4 +1,4 @@
-module.exports = function(config) {
+module.exports = function (config) {
   config.set({
     basePath: '',
     frameworks: ['jasmine', '@angular-devkit/build-angular'],
@@ -7,44 +7,35 @@ module.exports = function(config) {
       require('karma-chrome-launcher'),
       require('karma-jasmine-html-reporter'),
       require('karma-coverage'),
-      require('karma-junit-reporter'), // Ajouter le plugin junit reporter pour GitLab
       require('@angular-devkit/build-angular/plugins/karma')
     ],
     client: {
-      clearContext: false // Permet de ne pas effacer les résultats des tests dans la fenêtre du navigateur
+      jasmine: {
+        // you can add configuration options for Jasmine here
+        // the possible options are listed at https://jasmine.github.io/api/edge/Configuration.html
+        // for example, you can disable the random execution with `random: false`
+        // or set a specific seed with `seed: 4321`
+      },
+      clearContext: false // leave Jasmine Spec Runner output visible in browser
+    },
+    jasmineHtmlReporter: {
+      suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage'),
+      dir: require('path').join(__dirname, './coverage/'),
       subdir: '.',
       reporters: [
         { type: 'html' },
         { type: 'text-summary' }
       ]
     },
-    reporters: ['progress', 'kjhtml', 'junit'], // Ajout du reporter junit
-    junitReporter: {
-      outputDir: 'test-results',  // Répertoire où les résultats seront sauvegardés
-      outputFile: 'test-results.xml',  // Nom du fichier XML de sortie
-      useBrowserName: false        // Ne pas inclure le nom du navigateur dans le rapport
-    },
+    reporters: ['progress', 'kjhtml'],
     port: 9876,
     colors: true,
     logLevel: config.LOG_INFO,
-    autoWatch: false, // Désactive le mode watch pour l'exécution en CI
-    browsers: ['ChromiumHeadlessCI'],  // Utilise ChromiumHeadlessCI
-    singleRun: true, // Les tests s'exécutent une seule fois
-    restartOnFileChange: false,
-    customLaunchers: {
-      ChromiumHeadlessCI: {
-        base: 'ChromiumHeadless',
-        flags: [
-          '--no-sandbox',
-          '--headless',
-          '--disable-gpu',
-          '--disable-dev-shm-usage',
-          '--disable-web-security'
-        ]
-      }
-    }
+    autoWatch: false,
+    browsers: ['ChromeHeadless'],
+    singleRun: true,
+    restartOnFileChange: false
   });
 };
